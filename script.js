783ea534d84fcc16fa92ac84b383523276d4a404
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-09-11 12:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //your JS code here. If required.
-for(let i = 1; i <= 100; i++){
-	if(i % 15 === 0){
-		alert('FizzBuzz');
-	}
-	else if(i % 3 === 0){
-		alert('Fizz');
-	}
-	else if(i % 5 === 0){
-		alert('Buzz');
-	}
-	else{
-		alert(i);
-	}
-	alert("\n");
+let output = ''; 
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 15 === 0) {
+    output += 'FizzBuzz\n';
+  } else if (i % 3 === 0) {
+    output += 'Fizz\n';
+  } else if (i % 5 === 0) {
+    output += 'Buzz\n';
+  } else {
+    output += i + '\n';
+  }
 }
+
+alert(output);
